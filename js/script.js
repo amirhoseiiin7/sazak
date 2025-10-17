83bf711fae54172b604a4bef09bc837413e0f6f9
amirhoseiiin7 <a.naghiar@gmail.com>
2025-10-18 01:00:43 +0330

    Implement smooth scrolling for navigation links
    Added smooth scrolling functionality for navigation links.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,37 @@
 const yearEL = document.querySelector(".year");
 const currentyear = new Date().getFullYear();
 yearEL.textContent = currentyear;
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link")) {
+      headerEL.classList.toggle("open-nav");
+    }
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // MAKE MOBILE NAV WORK
 
@@ -13,6 +44,7 @@ const btnNavEL = document.querySelector(".btn-mobile-nav");
 btnNavEL.addEventListener("click", function () {
   headerEL.classList.toggle("open-nav");
 });
+
 /////////////////////////////////////////////////////////////
 
 const sectionHeroEl = document.querySelector(".hero-section");
@@ -35,6 +67,8 @@ const obs = new IntersectionObserver(
     root: null,
     threshold: 0,
     rootMargin: "-80px",
-  }
+  },
 );
 obs.observe(sectionHeroEl);
+
+/////////////////////////////////////////////////////////////////
